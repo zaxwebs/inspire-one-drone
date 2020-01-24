3d0ca102fe38b83fbfd617ab168e494fe94bdbde
zaxwebs <zifidigital@gmail.com>
2020-01-24 07:43:50 +0530

    Initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,12 @@
 import $ from 'jquery'
 import 'bootstrap'
+import Rellax from 'rellax'
+
+const rellax = new Rellax('.zax', {
+	speed: 4,
+	center: false,
+	wrapper: null,
+	round: true,
+	vertical: true,
+	horizontal: false
+})
